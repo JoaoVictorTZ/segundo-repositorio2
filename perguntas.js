cb1d7d0b50f26b69e259f83490181c8f5f6a3c28
JoaoVictorTZ <j.zanotto@escola.pr.gov.br>
2025-05-23 22:49:46 -0300

    23/05/2025 (2)

---
+++ perguntas.js
@@ -0,0 +1,17 @@
+criaCartao(
+    Programação
+    O que é Python?
+    O Python é uma linguagem de programação
+)
+
+criaCartao(
+    Geografia
+    Qual a capital da França?
+    A capital da França é Paris
+)
+
+criaCartao(
+    JOGOS
+    ULTRAKILL É BOM?
+    PRA KRL
+)
